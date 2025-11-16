4ad55027aae277ad6250bbc3fae5c3d2fa81a812
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-11-16 20:18:05 +0000

    Testing C's newline character for the printf function within pun

--- chapter_02/pun.c
+++ chapter_02/pun.c
@@ -1,7 +1,9 @@
+//My first C program
 #include <stdio.h>
 
 int main(void)
 {
-    printf("To C, or not to C; that is the question. \n");
+    printf("To C, or not to C;");
+    printf(" that is the question.");
     return 0;
 }

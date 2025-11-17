29d23673da7ca8d0e40995ec58b0946517a49fc5
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-11-17 14:23:30 +0000

    created a test file for exercise 2.2

---
+++ chapter_02/test.c
@@ -0,0 +1,19 @@
+//A file for Testing Outputs from the Book
+#include <stdio.h> //Directives
+
+int main(void)
+{
+    //Statements
+
+    printf("Parkinson's Law: \nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion. \n");
+    return 0;
+}
+
+// A) Identify the directives and statements in this program
+        //See Comments
+// B) What output does the program produce.
+        //lINE 1: Parkinson's Law: 
+        //Line 2: Work expands so as to fill the time
+        //Line 3: available for its completion

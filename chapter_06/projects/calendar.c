9204cf81de0b4235d351cc29ac25433b813e6020
ragnoros <carter.ormston@icloud.com>
2025-12-29 17:02:50 +0000

    Testing arch linux pushing

--- chapter_06/projects/calendar.c
+++ chapter_06/projects/calendar.c
@@ -1,26 +1,28 @@
-// Write a program that prints a calendar based on two inputs from the user. The number of days and the day in which the month starts.
+// Write a program that prints a calendar based on two inputs from the user. The
+// number of days and the day in which the month starts.
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-int main(void)
-{
+int main(void) {
 
-    int days, daystart;
-    bool run = true;
+  int days, daystart;
 
-    while (run)
-    {
-        printf("\nPlease enter the number of days: ");
-        scanf("%d", &days);
+  printf("\nPlease enter the number of days: ");
+  scanf("%d", &days);
 
-        printf("\nPlease enter the starting day (1=Sun 7=Sat): ");
-        scanf("%d", &daystart);
+  printf("\nPlease enter the starting day (1=Sun 7=Sat): ");
+  scanf("%d", &daystart);
 
-        if (days >= 1 && days <= 31 && daystart >= 1 && daystart <= 7)
-        {
-        }
+  if (days >= 1 && days <= 31 && daystart >= 1 && daystart <= 7) {
+    for (int i = 1; i <= days; i++) {
+
+      printf("%.2d ", i);
+      if (i % 7 == 0) {
+        printf("\n");
+      }
     }
-
-    return 0;
+  }
+  printf("\n");
+  return 0;
 }

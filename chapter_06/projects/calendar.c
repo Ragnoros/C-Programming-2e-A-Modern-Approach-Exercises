4799a339f7cd3d36ecf94ac0fa2dbe7d013a04a8
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-12-31 20:39:35 +0000

    Completed Chapter 6 Programming Project: Calendar Program Completed

--- chapter_06/projects/calendar.c
+++ chapter_06/projects/calendar.c
@@ -17,11 +17,17 @@ int main(void)
 
   if (days >= 1 && days <= 31 && daystart >= 1 && daystart <= 7)
   {
+
+    for (int i = 1; i < daystart; i++)
+    {
+      printf("   ");
+    }
+
     for (int i = 1; i <= days; i++)
     {
 
-      printf("%.2d ", i);
-      if (i % 7 == 0)
+      printf(" %.2d", i);
+      if (((i + daystart) - 1) % 7 == 0)
       {
         printf("\n");
       }

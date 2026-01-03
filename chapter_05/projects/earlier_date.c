e5780a23ee358f9f3a8a4177c4d9e046953fbf24
Lorontal <137430073+Lorontal@users.noreply.github.com>
2026-01-03 22:28:16 +0000

    Completed Chapter 6 Programming Project: Updating Chapter 05 Earlier Date adding a loop (not completed)

--- chapter_05/projects/earlier_date.c
+++ chapter_05/projects/earlier_date.c
@@ -1,36 +1,41 @@
 // Write a program that takes two dates from a user in mm/dd/yy format and evaluates which is earlier.
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
 
-    int mon1, day1, year1;
-    int mon2, day2, year2;
-    int date1_conv, date2_conv;
+    int month, day, year, date_conv, earliest = 0;
+    bool run = true;
 
-    printf("Please Enter the first date: ");
-    scanf("%d / %d / %d", &mon1, &day1, &year1);
-
-    printf("Please Enter the second date: ");
-    scanf("%d / %d / %d", &mon2, &day2, &year2);
-
-    date1_conv = (year1 * 10000) + (mon1 * 100) + day1;
-    date2_conv = (year2 * 10000) + (mon2 * 100) + day2;
-
-    if (date1_conv == date2_conv)
+    while (run)
     {
-        printf("%02d/%02d/%02d and %02d/%02d/%02d are the same date", mon1, day1, year1, mon2, day2, year2);
+        printf("Please enter a date to compare (0/0/0 to exit): ");
+        scanf("%d / %d / %d", &month, &day, &year);
+
+        if (!month && !day && !year)
+        {
+            run = false;
+            break;
+        }
+
+        if (!earliest)
+        {
+            earliest = (year * 10000) + (month * 100) + day;
+        }
+        else
+        {
+            date_conv = (year * 10000) + (month * 100) + day;
+        }
+
+        if (date_conv < earliest)
+        {
+            earliest = date_conv;
+        }
     }
 
-    if (date1_conv < date2_conv)
-    {
-        printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", mon1, day1, year1, mon2, day2, year2);
-    }
-    else if (date2_conv < date1_conv)
-    {
-        printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d", mon2, day2, year2, mon1, day1, year1);
-    }
+    printf("The earliest date is: %d", earliest);
 
     return 0;
 }

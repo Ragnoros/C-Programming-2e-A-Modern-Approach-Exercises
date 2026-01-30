4e72d4099b08ff37aac78adb292da8bb271d9eb5
Lorontal <137430073+Lorontal@users.noreply.github.com>
2026-01-30 00:18:40 +0000

    Completed Chapter 7 Programming Project: Scrabble program completed

---
+++ chapter_07/projects/scrabble.c
@@ -0,0 +1,72 @@
+// Write a program that mimics the game Scrabble.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+    bool run = true;
+    short ch;
+    short score = 0;
+
+    printf("Enter a word: ");
+
+    while (run)
+    {
+        ch = getchar();
+        ch = toupper(ch);
+
+        switch (ch)
+        {
+        case '\n':
+            run = false;
+            break;
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'L':
+        case 'N':
+        case 'O':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+            score += 1;
+            break;
+        case 'D':
+        case 'G':
+            score += 2;
+            break;
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P':
+            score += 3;
+            break;
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y':
+            score += 4;
+            break;
+        case 'K':
+            score += 5;
+            break;
+        case 'J':
+        case 'X':
+            score += 8;
+            break;
+        case 'Q':
+        case 'Z':
+            score += 10;
+            break;
+        }
+    }
+
+    printf("Scrabble value: %d", score);
+
+    return 0;
+}

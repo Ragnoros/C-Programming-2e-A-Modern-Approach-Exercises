20d7b2500a2a01cad8d3e66ce2a7503a217db87c
ragnoros <carter.ormston@icloud.com>
2026-05-25 23:57:24 +0100

    Chapter_07 Programming Projects: First and Last name formatting

---
+++ chapter_07/projects/first_last_name.c
@@ -0,0 +1,20 @@
+// Write a program takes a first name and last name entered by the user and displays the last name, a comma, and the first initial.
+// followed by a period
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+    int last_name, initial;
+
+    while (getchar() != EOF && getchar() != '\n')
+    {
+        switch (getchar())
+        {
+        }
+    }
+
+    return 0;
+}
